99f4b5684a53def954220ca65e9e803f43c8de57
zack <zackshen0526@gmail.com>
2015-06-06 21:53:38 +0800

    add vim submodule

--- config.sh
+++ config.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
 
-CURRENT_DIR=$(pwd)
-
-# ************************
-# Tmux
-# ************************
-tmux_config() {
-    # link tmux config file
-    tmux_conf_path=${CURRENT_DIR}'/tmux/tmux.conf'
-
-    if [ -f "${tmux_conf_path}" ]; then
-        echo "remove ~/.tmux.conf, link new config file at "${CURRENT_DIR}'/tmux/tmux.conf'
-        rm -rf ~/.tmux.conf
-    fi
-
-    ln -s $tmux_conf_path ~/.tmux.conf
-    echo 'link tmux config file finished'
-}
-
-tmux_config
+. ./tmux/install.sh
+. ./vim/install.sh
